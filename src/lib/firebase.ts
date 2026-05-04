59a03c9d641ffa8777ea93fbdf3b436535ce73ef
nguyenphanngocthanh2k5-source <nguyenphanngocthanh2k5@gmail.com>
2026-05-04 06:05:38 +0000

    luu firebase lan 2

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -23,17 +23,13 @@ import {
 import { Plan, AppSettings } from "../types";
 
 const firebaseConfig = {
-  apiKey: String(import.meta.env.VITE_FIREBASE_API_KEY || ""),
-  authDomain:
-    import.meta.env.VITE_FIREBASE_AUTH_DOMAIN ||
-    (typeof window !== "undefined" ? window.location.host : "5381fe69-513c-4292-8793-e5e50b6af9a5-00-u8900ybbdbnl.pike.replit.dev"),
-  projectId: String(import.meta.env.VITE_FIREBASE_PROJECT_ID || ""),
-  storageBucket: import.meta.env.VITE_FIREBASE_PROJECT_ID
-    ? `${import.meta.env.VITE_FIREBASE_PROJECT_ID}.firebasestorage.app`
-    : undefined,
+  apiKey: "AIzaSyAI2wBxUR9V5OHr1fVNHJbNv0ReUqxjOww",
+  authDomain: "wadebicycle.firebaseapp.com",
+  projectId: "wadebicycle",
+  storageBucket: "wadebicycle.firebasestorage.app",
   messagingSenderId: "365678601546",
-  appId: String(import.meta.env.VITE_FIREBASE_APP_ID || ""),
-  measurementId: "G-66ZD4J6QX3",
+  appId: "1:365678601546:web:40c042ab0961b693ec0db3",
+  measurementId: "G-66ZD4J6QX3"
 };
 
 const isNewApp = getApps().length === 0;
